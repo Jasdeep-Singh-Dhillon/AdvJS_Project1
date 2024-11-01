d46fb0ba588182cd2a117faf8004dc09364ac93c
Jasdeep Singh Dhillon <jdps2867@gmail.com>
2024-10-31 23:40:59 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// class Task {
+//     constructor(title, description, assigned, dateCreated, status) {
+//         this.title = title;
+//         this.description = description;
+//         this.assigned = assigned;
+//         this.dateCreated = dateCreated;
+//         this.status = status;
+//     }
+// }
+
+// const task = new Task("Test1", "Description of Test1", "Harjeet", new Date(), "1");
+// const task2 = new Task("Test2", "Changed description very helpful", "Jasdeep", new Date(), "0");
+
+
+// const tasks = [];
+// tasks.push(task);
+// tasks.push(task2);
+// console.log(tasks);
+
+// localStorage["tasks"] = JSON.stringify(tasks);
+
+// const tasks = JSON.parse(localStorage["tasks"]);
+// console.log(tasks);
