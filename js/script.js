c4d25621d0f34d3a0f635f1ecf6d0264f7cfe7cb
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2024-11-04 21:07:15 -0800

    Populating from local Storage

--- js/script.js
+++ js/script.js
@@ -3,35 +3,35 @@
 class Task {
 
     constructor(title, desc, assigned, dateCreated, status) {
-        if(!title) {
+        if (!title) {
             title = "No title";
         }
         // Title of the task
         this.title = title;
 
-        if(!desc) {
+        if (!desc) {
             desc = "No Description";
         }
         // Description of the task / Can be changed to array for checkbox
         this.desc = desc;
 
-        if(!assigned) {
+        if (!assigned) {
             assigned = "Not Assigned";
         }
         // User assigned to the task
         this.assigned = assigned;
 
-        if(!dateCreated || new Date(dateCreated) == "Invalid Date") {
+        if (!dateCreated || new Date(dateCreated) == "Invalid Date") {
             dateCreated = new Date();
         }
         // When the task was created 
-        this.dateCreated = dateCreated;
-        if(!status) {
+        this.dateCreated = new Date(dateCreated);
+        if (!status) {
             status = 0;
         }
-        if(isNaN(status)) {
+        if (isNaN(status)) {
             status = parseInt(status);
-            if(isNaN(status)) {
+            if (isNaN(status)) {
                 status = 0;
             }
         }
@@ -73,6 +73,44 @@ class Task {
         return this.status;
     }
 
+    toHTML = () => {
+        let checked = "";
+        let completed = "";
+        if (this.status) {
+            checked = "checked";
+            completed = "complete";
+        }
+        const taskHTML = `<div class="task ${completed}">
+      <div class="title flex flex-align">
+        <input type="checkbox" name="progress" id="progress" ${checked}>
+        <label for="progress">${this.title}</label>
+      </div>
+      <div class="desc">
+        ${this.desc}
+      </div>
+
+      <div class="taskinfo flex">
+        <div class="assigned flex">
+          <div class="person flex flex-center">
+            ${this.assigned}
+          </div>
+        </div>
+        <div class="date flex flex-center">
+          ${this.dateCreated.toLocaleDateString()}
+        </div>
+        <div class="edit flex flex-align">
+            Edit
+        </div>
+        <div class="delete flex flex-align">
+            Delete
+        </div>
+
+      </div>
+
+    </div>`;
+        return taskHTML;
+    }
+
 }
 
 let tasks = [];
@@ -82,14 +120,22 @@ if (localStorage?.tasks) {
         tasks = localStorage.tasks;
         tasks = JSON.parse(tasks);
 
-        for(let task in tasks) {
-            task[i] = new Task(task[i].title, task.description, task.assigned, task.dateCreated, task.status);
-            console.log(task.getTitle());
+        for (let task of tasks) {
+            task = new Task(task.title, task.desc, task.assigned, task.dateCreated, task.status);
+            console.log(task);
+            document.querySelector('main').innerHTML += task.toHTML();
         }
     }
     catch (err) {
         console.log("Error: ", err);
     }
 }
+// let tasks = [];
 
-console.log(tasks);
+// for(let i = 0; i < 10; i++) {
+//     let task = new Task(`Title ${i+1}`, `Description of task ${i+1}`, `Person ${i+1}`, new Date(), Math.round(Math.random()));
+//     tasks.push(task);
+
+// }
+
+// localStorage.tasks = JSON.stringify(tasks);
