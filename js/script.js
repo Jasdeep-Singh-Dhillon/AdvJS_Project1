cdb85c53595b51fff1eda890750c136c4f846194
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2024-11-04 20:25:21 -0800

    Created task template and updated css for the task classes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 "use strict";
 
+let tasks = [];
+
+if(localStorage?.tasks) {
+    tasks = JSON.parse(localStorage[tasks]);
+}
+
 class Task {
     constructor(title, description, assigned, dateCreated, status) {
         // Title of the task
@@ -8,40 +14,19 @@ class Task {
         // Description of the task / Can be changed to array for checkbox
         this.description = description;
 
-        // User / users assigned to the task
+        // User assigned to the task
         this.assigned = assigned;
 
         // When the task was created 
         this.dateCreated = dateCreated;
 
         // Status (Number)
-        // 0 = Starting
-        // 1 = In-Progress
-        // 2 = Completed 
+        // 0 = Incomplete
+        // 1 = Complete
         this.status = status;
     }
 
-    // setTitle = (title) => {
-    //     this.title = title;
-    // }
-
-    // setDescription = (desc) => {
-    //     this.description = desc;
-    // }
-
-    // setStatus = (status) => {
-    //     this.status = status;
-    // }
-
-    // setAssigned = (user) => {
-    //     this.assigned = user;
-    // }
 }
-
-/*
-tasks:"[{"title":"Test1","description":"Description of Test1","assigned":"Harjeet","dateCreated":"2024-11-01T03:36:04.881Z","status":"1"},{"title":"Test2","description":"Changed description very helpful","assigned":"Jasdeep","dateCreated":"2024-11-01T03:36:04.881Z","status":"0"}]"
-*/
-
 
 const task = new Task("Test1", "Description of Test1", "Harjeet", new Date(), 1);
 
@@ -57,10 +42,5 @@ console.log(task);
 // console.log(tasks);
 
 // localStorage["tasks"] = JSON.stringify(tasks);
-
-
-
-const tasks = JSON.parse(localStorage["tasks"]);
-tasks[0].title = "Changed to check if this works";
 
 console.log(tasks);
